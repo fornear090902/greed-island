cd04bea2f5d412f23fc06c8c4b7fa703137e4098
fornear <fornear.dev@gmail.com>
2024-02-02 13:20:50 +0000

    fix layout

--- src/greed-island/app/page.tsx
+++ src/greed-island/app/page.tsx
@@ -1,14 +1,9 @@
-import { Header } from "./Header"
-import { User } from "@/domain/User"
-import { uuidv7 } from "uuidv7"
 
 export default function Home() {
 
-  const user = new User(uuidv7(), 'ゲスト', 'です男')
-
   return (
     <div>
-      <Header user={user} />
+      Home
     </div>
   )
 }
---
+++ src/greed-island/components/Footer.tsx
@@ -0,0 +1,6 @@
+
+export function Footer () {
+    return <div className="border-t-2 border-t-gray-300 bg-gray-200 sticky bottom-0 px-4 py-2 text-center text-gray-500">
+        Greed Island
+    </div>
+}
