4b8273f37513f9d87e82c163b47e8c25e24f2090
fornear <fornear.dev@gmail.com>
2024-02-02 16:37:05 +0000

    wip

---
+++ src/greed-island/domain/buttle/Buttle.ts
@@ -0,0 +1,34 @@
+import { Player } from "../player/Player";
+import { ButtleSetup, IButtleSetup } from "./ButtleSetup";
+
+export interface IButtle {
+    setup(player1: Player, player2: Player): void;
+    start(): void;
+    showResult(): void;
+    finish(): void;
+}
+
+export class Buttle implements IButtle {
+
+    constructor(
+        private player1: Player,
+        private player2: Player
+    ) {}
+
+    setup(player1: Player, player2: Player): IButtle {
+        const setup: IButtleSetup = new ButtleSetup();
+        return setup.setup(player1, player2);
+    }
+
+    start(): void {
+        // TODO: Implement start method
+    }
+
+    showResult(): void {
+        // TODO: Implement showResult method
+    }
+
+    finish(): void {
+        // TODO: Implement finish method
+    }
+}
