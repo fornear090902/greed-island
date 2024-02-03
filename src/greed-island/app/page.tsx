19c9c12e41cf3b3d1985003952f9e8efe07f4241
fornear <fornear.dev@gmail.com>
2024-02-03 12:47:11 +0000

    add github auth

--- src/greed-island/app/page.tsx
+++ src/greed-island/app/page.tsx
@@ -1,18 +1,26 @@
+'use client'
+
 import { Signup } from "@/components/Signup"
 import { Status } from "@/components/player/template/Status"
 import { Player } from "@/domain/player/Player"
+import { useSession } from "next-auth/react"
 
 interface Props {
   player: Player | null
 }
 
 export default function Home(props: Props) {
+  const {data: session, status} = useSession()
+
+  if (status === "loading") {
+    return <div>Loading...</div>
+  }
 
   return (
     <>
       {
-        props.player
-          ? <Status player={props.player}></Status>
+        session
+          ? <div>サインイン済</div>
           : <Signup />
       }
     </>
