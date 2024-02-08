0678a75f5c1c647d11a39451e19a171e9173ccc2
fornear <fornear.dev@gmail.com>
2024-02-08 15:09:39 +0000

    fix

--- src/greed-island/app/api/trpc/[trpc]/route.ts
+++ src/greed-island/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,6 @@
 import { appRouter } from "@/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import * as trpcNext from "@trpc/server/adapters/next";
 import { NextRequest } from "next/server";
-
-/* const handler = trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: () => ({}),
-}) */
 
 const handler = (req: NextRequest) => {
   return fetchRequestHandler({
--- src/greed-island/lib/trpc.ts
+++ src/greed-island/lib/trpc.ts
@@ -1,7 +1,6 @@
 import { AppRouter } from "@/server";
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
-import { devtoolsLink } from "trpc-client-devtools-link";
 
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
@@ -12,9 +11,6 @@ export const trpc = createTRPCNext<AppRouter>({
   config(opts) {
     return {
       links: [
-        devtoolsLink({
-          enabled: true,
-        }),
         httpBatchLink({ url: `${getBaseUrl()}/api/trpc` }),
       ],
     };
