15ae52e158e889864026d1fcfac46d8722003bbe
fornear <fornear.dev@gmail.com>
2024-02-09 14:15:36 +0000

    fix page transition

--- src/greed-island/app/entrance/page.tsx
+++ src/greed-island/app/entrance/page.tsx
@@ -1,14 +1,13 @@
 'use client';
 
-import { signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { trpc } from "@/lib/trpc";
 import { useRouter } from "next/navigation";
 
 
 export default function Page() {
     const { data: session, status } = useSession();
-    const [ path, setPath ] = useState<string>("/entrance/signin");
+    const [path, setPath] = useState<string>("/main");
     const router = useRouter();
 
     useEffect(() => {
@@ -26,10 +25,19 @@ export default function Page() {
     return (
         <div className="flex flex-col justify-evenly">
             <div>
-                <button
-                    onClick={start}
-                    className="px-6 py-4 rounded-lg text-3xl bg-white hover:bg-slate-200 border-2 border-slate-950 text-slate-950"
-                >Start</button>
+                {status === 'loading' && <div>loading...</div>}
+                {status === 'unauthenticated' && (
+                    <button
+                        onClick={() => signIn("google", { callbackUrl: "/entrance" })}
+                        className="px-6 py-4 rounded-lg text-3xl bg-white hover:bg-slate-200 border-2 border-slate-950 text-slate-950"
+                    >Googleでログイン</button>
+                )}
+                {status === 'authenticated' && (
+                    <button
+                        onClick={start}
+                        className="px-6 py-4 rounded-lg text-3xl bg-white hover:bg-slate-200 border-2 border-slate-950 text-slate-950"
+                    >Start</button>
+                )}
             </div>
             <div></div>
             <div></div>
