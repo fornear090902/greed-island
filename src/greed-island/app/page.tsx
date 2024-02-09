7700aff90fdb7290c5efc2fde0ab6d55c8bf1e77
fornear <fornear.dev@gmail.com>
2024-02-09 14:25:37 +0000

    delete unnecessary components

--- src/greed-island/app/page.tsx
+++ src/greed-island/app/page.tsx
@@ -1,16 +1,3 @@
-"use client";
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  const { status } = useSession();
-
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
-  if (status === "authenticated") {
-    return <div>ログイン済</div>;
-  }
-
-  redirect("/signin");
+export default function Page() {
+  return <div>ここにはなにもないよ</div>
 }
--- src/greed-island/components/Footer.tsx
+++
@@ -1,3 +0,0 @@
-export function Footer() {
-  return <div className="footer">Greed Island</div>;
-}
